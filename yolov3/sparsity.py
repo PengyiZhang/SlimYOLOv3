7424fa54fa6a9383ec3eba2f2f753c73cc852c6c
PengyiZhang <zhangpybit@gmail.com>
2019-07-26 00:18:19 +0800

    develop

--- yolov3/sparsity.py
+++
@@ -1,6 +0,0 @@
-# additional subgradient descent on the sparsity-induced penalty term
-# x_{k+1} = x_{k} - \alpha_{k} * g^{k}
-def updateBN(scale, model):
-    for m in model.modules():
-        if isinstance(m, nn.BatchNorm2d):
-            m.weight.grad.data.add_(scale*torch.sign(m.weight.data))  # L1
